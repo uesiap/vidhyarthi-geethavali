ea24213a9bf5f1fc5153fc0c85186ec5ef0a2f19
UESI AP <rahul360info@gmail.com>
2026-07-04 17:59:32 +0530

    Update sw.js

--- assets/pwa/sw.js
+++ assets/pwa/sw.js
@@ -1,5 +1,5 @@
 if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('/vidhyarthi-geethavali/assets/pwa/service-worker.js?v={{ site.github.build_revision }}')
+    navigator.serviceWorker.register('/vidhyarthi-geethavali/assets/pwa/service-worker.js')
         .then(reg => {
             console.log('SW registered:', reg.scope);
         })
